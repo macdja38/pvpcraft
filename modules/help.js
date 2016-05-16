907627aae064775ed1882c6a4f34882f2059489e
Macdja38 <jakeincanada@icloud.com>
2016-05-16 13:59:50 -0400

    don't log help commands as errors.

--- modules/help.js
+++ modules/help.js
@@ -20,32 +20,9 @@ module.exports = class help {
 
     onCommand(msg, command, perms, l) {
         console.log("Help initiated");
-        console.log("RED RED RED".red);
         //permissions have not yet been added, this is a preliminary version of the help command. Final version will be dynamic.        
         if (command.command === "help" || command.commandnos === "command") {
             msg.reply("Help can be found at https://pvpcraft.ca/pvpbot");
-            if(msg.channel.server) {
-                this.raven.captureMessage("Someone needed help!", {
-                    level: "info",
-                    user: msg.author,
-                    extra: {
-                        channel: msg.channel.id,
-                        channel_name: msg.channel.name,
-                        server: msg.channel.server.id,
-                        server_name: msg.channel.server.name
-                    }
-                });
-            } else {
-                this.raven.captureMessage("Someone needed help!", {
-                    level: "info",
-                    user: msg.author,
-                    extra: {
-                        channel: msg.channel.id,
-                        channel_name: msg.channel.name
-                    }
-                });
-                return true;
-            }
             return true;
         }
         return false;
