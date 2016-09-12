f9bfc5b5ccaf077c16b0af7842f49a2c43700386
macdja38 <jakeincanada@icloud.com>
2016-09-12 04:56:51 -0400

    Add a way to find info about the bot when it's sharded.

---
+++ middleware/shardedInfo.js
@@ -0,0 +1,121 @@
+/**
+ * Created by macdja38 on 2016-09-01.
+ */
+"use strict";
+
+var Utils = require('../lib/utils');
+var utils = new Utils();
+
+var StandardDB = require('../lib/standardDB');
+
+module.exports = class shardedInfo {
+  constructor(e) {
+    this._client = e.client;
+    this._timer = false;
+    this._ready = false;
+    this._standardDB = false;
+    e.conn.then((conn)=> {
+      this._standardDB = new StandardDB("shards", this._getArray(parseInt(process.env.shards)), conn);
+      this._ready = this._standardDB.reload();
+      console.log(this._ready);
+    }).catch(error => console.error(error));
+  }
+
+  /**
+   * Get's called every time the bot connects, not just the first time.
+   */
+  onReady() {
+    if (this._timer) clearInterval(this._timer);
+    this._timer = setInterval(this._updateDB.bind(this), 10000);
+  }
+
+  _getArray(n) {
+    return [...new Array(n).keys()].map(val => val.toString());
+  }
+
+  _updateDB() {
+    if (!this._ready || !this._ready.then) return;
+    this._ready.then(()=> {
+      this._standardDB.set(null,
+        {
+          servers: this._client.servers.length,
+          connections: this._client.voiceConnections.length,
+          playing: this._client.voiceConnections.filter(c => c.playing).length,
+          users: this._client.users.length,
+          shards: process.env.id,
+          lastUpdate: Date.now(),
+        }, {server: process.env.id})
+    }).catch(error => console.error(error))
+  }
+
+  /**
+   * Get's called every time the bot disconnects.
+   */
+  onDisconnect() {
+    if (this._timer) clearInterval(this._timer);
+  }
+
+  /**
+   * Get's called every message.
+   * @param msg
+   * @param perms
+   */
+  onMessage(msg, perms) {
+  }
+
+  /**
+   * Get's called every command.
+   * @param msg
+   * @param command
+   * @param perms
+   * @param l
+   */
+  onCommand(msg, command, perms, l) {
+  }
+
+  /**
+   * get's called every Message, (unless a previous middleware on the list override it.) can modify message.
+   * @param msg
+   * @param perms
+   * @returns msg that will be passed to modules and other middleware
+   */
+  changeMessage(msg, perms) {
+    return msg;
+  }
+
+  /**
+   * get's called every Command, (unless a previous middleware on the list override it.) can modify message.
+   * @param msg
+   * @param command
+   * @param perms
+   * @param l
+   * @returns command || Boolean object (may be modified.)
+   */
+  changeCommand(msg, command, perms, l) {
+    try {
+      if (command.command === "getshardedinfo") {
+        if (!this._standardDB.data) msg.reply("Sorry db connection not ready yet");
+        console.log(typeof(this._standardDB.data));
+        console.log(this._standardDB.data);
+        let serverData = [];
+        for (let key in this._standardDB.data) {
+          if (key < process.env.shards && this._standardDB.data.hasOwnProperty(key)) {
+            serverData[key] = this._standardDB.data[key]
+          }
+        }
+        let shardsOnline = serverData.filter(s => Date.now() - s.lastUpdate < 30000).length;
+        console.log(serverData);
+        let serverCount = serverData.map(s => s.servers).reduce((total, num) => total + num, 0);
+        let connections = serverData.map(s => s.connections).reduce((total, num) => total + num, 0);
+        let playing = serverData.map(s => s.playing).reduce((total, num) => total + num, 0);
+        let users = serverData.map(s => s.users).reduce((total, num) => total + num, 0);
+        msg.reply(`\`\`\`xl\nshards online: ${shardsOnline}/${process.env.shards}\nservers: ${serverCount}\nconnections: ${connections}\nplaying: ${playing}\nusers: ${users}\n\`\`\``);
+        return false;
+      }
+      return command;
+    } catch(error) {
+      console.error(error);
+      return command;
+    }
+  }
+};
