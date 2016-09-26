943e44ec3be08402e5904b0109400ff99b25d56e
macdja38 <jakeincanada@icloud.com>
2016-09-26 00:59:03 -0400

    correct object merging

--- modules/feedManager.js
+++ modules/feedManager.js
@@ -60,7 +60,7 @@ module.exports = class feedManager {
         return true;
       }
       this._feeds.set(adding, command.args[1].toLowerCase(), channel.id, channel.server.id);
-
+      msg.reply(`${adding ? "Starting" : "Stopping"} ${command.args[1].toLowerCase()} in channel ${channel.mention()}`);
 
       //return true, which tells the command dispatcher that we processed the command.
       return true;
