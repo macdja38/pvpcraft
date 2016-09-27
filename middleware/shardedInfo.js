d78ded2d029002e0059292397dcb27aa79a13468
macdja38 <jakeincanada@icloud.com>
2016-09-27 19:56:41 -0400

    check the last time a shard recieved a message

--- middleware/shardedInfo.js
+++ middleware/shardedInfo.js
@@ -6,18 +6,21 @@
 var Utils = require('../lib/utils');
 var utils = new Utils();
 
+var request = require('request');
+
 var StandardDB = require('../lib/standardDB');
 
 module.exports = class shardedInfo {
   constructor(e) {
+    this._auth = e.auth;
     this._client = e.client;
     this._timer = false;
     this._ready = false;
+    this._lastMessage = Date.now();
     this._standardDB = false;
     e.conn.then((conn)=> {
       this._standardDB = new StandardDB("shards", this._getArray(parseInt(process.env.shards || 1)), conn);
       this._ready = this._standardDB.reload();
-      console.log(this._ready);
     }).catch(error => console.error(error));
   }
 
@@ -44,6 +47,7 @@ module.exports = class shardedInfo {
           users: this._client.users.length,
           shards: parseInt(process.env.shards) || 1,
           lastUpdate: Date.now(),
+          lastMessage: this._lastMessage,
         }, {server: process.env.id ? process.env.id : "0"})
     }).catch(error => console.error(error))
   }
@@ -55,32 +59,61 @@ module.exports = class shardedInfo {
     if (this._timer) clearInterval(this._timer);
   }
 
-  /**
-   * Get's called every message.
-   * @param msg
-   * @param perms
-   */
-  onMessage(msg, perms) {
+  onServerCreated() {
+    if (!this._standardDB.data) return;
+    let serverData = [];
+    for (let key in this._standardDB.data) {
+      if (this._standardDB.data.hasOwnProperty(key) && parseInt(key) < parseInt(process.env.shards) || 1) {
+        serverData[parseInt(key)] = this._standardDB.data[key]
+      }
+    }
+    let serverCount = serverData.map(s => s.servers).reduce((total, num) => total + num, 0);
+    //this.updateCarbonitex(serverCount);
+    //this.updateAbal(serverCount);
   }
 
-  /**
-   * Get's called every command.
-   * @param msg
-   * @param command
-   * @param perms
-   * @param l
-   */
-  onCommand(msg, command, perms, l) {
+  onMessage() {
+    this._lastMessage = Date.now();
   }
 
-  /**
-   * get's called every Message, (unless a previous middleware on the list override it.) can modify message.
-   * @param msg
-   * @param perms
-   * @returns msg that will be passed to modules and other middleware
-   */
-  changeMessage(msg, perms) {
-    return msg;
+  updateAbal(servers) {
+    let token = this._auth.get("abalKey", false);
+    if(token && token.length > 1) {
+      request.post({
+        url: `https://bots.discord.pw/api/bots/${this._client.id}/stats`,
+        header: token,
+        body: {server_count: servers}
+      })
+    }
+  }
+
+  updateCarbonitex(servers) {
+    console.log("Attempting to update Carbon".green);
+    if (process.uptime() < 60) {
+      console.log("Not updating carbon to ensure all servers are loaded".green);
+      return;
+    }
+    if (key) {
+      request(
+        {
+          url: 'https://www.carbonitex.net/discord/data/botdata.php',
+          body: { key: this._auth.get("key"), servercount: servers },
+          json: true
+        },
+        function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+            console.log(body)
+          }
+          else if (error) {
+            console.error(error);
+          }
+          else {
+            console.error("Bad request or other");
+            console.error(response.body);
+          }
+        }
+      );
+    }
   }
 
   /**
@@ -94,9 +127,10 @@ module.exports = class shardedInfo {
   changeCommand(msg, command, perms, l) {
     try {
       if (command.command === "getshardedinfo") {
-        if (!this._standardDB.data) msg.reply("Sorry db connection not ready yet");
-        console.log(typeof(this._standardDB.data));
-        console.log(this._standardDB.data);
+        if (!this._standardDB.data) {
+          msg.reply("Sorry db connection not ready yet");
+          return true;
+        }
         let serverData = [];
         for (let key in this._standardDB.data) {
           if (this._standardDB.data.hasOwnProperty(key) && parseInt(key) < parseInt(process.env.shards) || 1) {
@@ -104,12 +138,12 @@ module.exports = class shardedInfo {
           }
         }
         let shardsOnline = serverData.filter(s => Date.now() - s.lastUpdate < 30000).length;
-        console.log(serverData);
+        let shardsReceivingMessages = serverData.filter(s => Date.now() - s.lastMessage < 60000).length;
         let serverCount = serverData.map(s => s.servers).reduce((total, num) => total + num, 0);
         let connections = serverData.map(s => s.connections).reduce((total, num) => total + num, 0);
         let playing = serverData.map(s => s.playing).reduce((total, num) => total + num, 0);
         let users = serverData.map(s => s.users).reduce((total, num) => total + num, 0);
-        msg.reply(`\`\`\`xl\nshards online: ${shardsOnline}/${process.env.shards || 1}\nservers: ${serverCount}\nconnections: ${connections}\nplaying: ${playing}\nusers: ${users}\n\`\`\``);
+        msg.reply(`\`\`\`xl\nshards online: ${shardsOnline}/${process.env.shards || 1}\nshards connected: ${shardsReceivingMessages}/${process.env.shards || 1}\nservers: ${serverCount}\nconnections: ${connections}\nplaying: ${playing}\nusers: ${users}\n\`\`\``);
         return false;
       }
       return command;
