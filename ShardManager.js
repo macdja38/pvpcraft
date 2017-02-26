f09979a30e0659c63d62da76836cef3fe87a7502
macdja38 <jakeincanada@icloud.com>
2017-02-26 01:39:22 -0500

    don't pass worker pids as sentry won't group the messages.

--- ShardManager.js
+++ ShardManager.js
@@ -89,7 +89,7 @@ module.exports = class ShardManager {
 
     cluster.on('exit', (deadWorker, code, signal) => {
       if (this.raven) {
-        this.raven.captureMessage(`worker ${deadWorker.process.pid} died with code ${code} and signal ${signal}`);
+        this.raven.captureMessage(`worker died with code ${code} and signal ${signal}`);
       }
       console.log(`worker ${deadWorker.process.pid} died with code ${code} and signal ${signal}`);
       this.restartQueue.push(deadWorker);
