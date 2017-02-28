532eaab6a283ebf12063d298bc5272b59a727775
macdja38 <jakeincanada@icloud.com>
2017-02-28 04:21:19 -0500

    switch to docma for documentation generation

---
+++ docma.js
@@ -0,0 +1,70 @@
+/**
+ * Created by macdja38 on 2017-02-28.
+ */
+"use strict";
+const Docma = require('docma');
+
+let config = {
+  app: {
+    title: 'pvpcraft',
+    base: '/pvpcraft/',
+    server: Docma.ServerType.GITHUB,
+  },
+  src: [
+    './lib/*.js',
+    './README.md',
+  ],
+  dest: './docs',
+  template: {
+    // Template-specific options
+    options: {
+      title: 'PvPCraft',
+      sidebar: true,
+      collapsed: false,
+      badges: true,
+      search: true,
+      navbar: true,
+      navItems: [
+        {
+          label: 'Documentation',
+          href: '#',
+          iconClass: 'ico-book'
+        },
+        {
+          label: 'Demos',
+          href: 'demos.html',
+          iconClass: 'ico-mouse-pointer'
+        },
+        {
+          label: 'Download',
+          iconClass: 'ico-md ico-download',
+          items: [
+            {
+              label: 'dev',
+              href: 'https://github.com/macdja38/pvpcraft/archive/dev.zip',
+            },
+            {
+              label: 'release',
+              href: 'https://github.com/macdja38/pvpcraft/archive/master.zip',
+            }
+          ]
+        },
+        {
+          label: 'GitHub',
+          href: 'https://github.com/macdja38/pvpcraft',
+          target: '_blank',
+          iconClass: 'ico-md ico-github'
+        }
+      ]
+    }
+  },
+};
+
+Docma.create()
+  .build(config)
+  .then(function () {
+    console.log('Documentation is built successfully.');
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
