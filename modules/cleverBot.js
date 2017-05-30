61eca060184a8277cf4378fe3b17a70b686dc3db
macdja38 <jakeincanada@icloud.com>
2017-05-29 22:48:22 -0400

    use custom branch of custom cleverbot.io lib

--- modules/cleverBot.js
+++ modules/cleverBot.js
@@ -5,7 +5,7 @@
 
 let utils = require('../lib/utils');
 
-let CleverBot = require('cleverbot.io');
+let CleverBotIO = require('better-cleverbot-io');
 
 class cleverBot {
   /**
@@ -56,32 +56,16 @@ class cleverBot {
       }
       msg.channel.sendTyping();
       let query = msg.content.replace(this.startRegex, "").replace(this.middleRegex, "CleverBot");
-      console.log(query);
       let bot;
       if (this.sessionMap.has(msg.channel)) {
         bot = this.sessionMap.get(msg.channel);
       } else {
-        let botInstance = new CleverBot(this.cleverUser, this.cleverKey);
-        bot = new Promise((resolve, reject) => {
-          botInstance.create((err, session) => {
-            if (err) {
-              reject(err);
-            } else {
-              botInstance.setNick(session);
-              resolve(botInstance);
-            }
-          });
-        });
+        let botInstance = new CleverBotIO({user: this.cleverUser, key: this.cleverKey, nick: msg.channel.id});
+        bot = botInstance.create();
         this.sessionMap.set(msg.channel, bot);
       }
       return bot.then((botInstance) => {
-        botInstance.ask(query, (err, response) => {
-          console.log(err, response);
-          if (err) {
-            msg.channel.createMessage(`${msg.author.mention}, Encountered error "${response}" when trying to query cleverbot`).catch(perms.getAutoDeny(msg));
-            this.raven.captureException(response);
-            return true;
-          }
+        return botInstance.ask(query).then((response) => {
           msg.channel.createMessage(msg.author.mention + ", " + utils.clean(response)).catch(perms.getAutoDeny(msg));
         });
       });
