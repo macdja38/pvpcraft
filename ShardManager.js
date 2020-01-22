4a9ddf2dcd0e378774d89f456709c54b6b1438a8
macdja38 <jakeincanada@icloud.com>
2020-01-22 03:06:31 -0500

    Send shard index of shards that die to sentry.

--- ShardManager.js
+++ ShardManager.js
@@ -96,10 +96,11 @@ module.exports = class ShardManager {
     }, 1000);
 
     cluster.on('exit', (deadWorker, code, signal) => {
+      const shardId = this.workers.indexOf(deadWorker + this.startShard);
       if (this.raven) {
-        this.raven.captureMessage(`worker died with code ${code} and signal ${signal}`, { extra: { shardId: this.workers.indexOf(deadWorker + this.startShard)}});
+        this.raven.captureMessage(`worker died with code ${code} and signal ${signal}`, { extra: { shardId }});
       }
-      console.log(`worker ${deadWorker.process.pid} died with code ${code} and signal ${signal}`);
+      console.log(`worker ${deadWorker.process.pid} died with code ${code} and signal ${signal}. Shard: ${shardId}`);
       this.restartQueue.push(deadWorker);
     });
 
