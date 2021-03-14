1909a98fa11d46c78a1999b98e3f59a5eca2bdf7
macdja38 <jakeincanada@icloud.com>
2021-03-13 23:34:02 -0500

    Make TaskQueue execute tasks on the shard that spawned them
    Note: It actually executes them on the shard that happens to have the guild that the task refers to at the time the task executes.

--- src/lib/TaskQueue.ts
+++ src/lib/TaskQueue.ts
@@ -7,42 +7,54 @@ import utils from "./utils";
 
 const maxRetries = 3;
 
-interface Task {
+interface RawTask {
   id?: string;
   retries?: number;
   action: string;
+  guildID: string;
   meta: any;
 }
 
-interface TaskWithID extends Task {
+interface TaskWithID extends RawTask {
   id: string;
+}
+
+interface ExtendedTask extends RawTask {
+  shardCount: number;
+  botID: string;
+  expireTime: ReturnType<BaseDB["r"]["epochTime"]>;
+  shiftedGuildID: number;
 }
 
 const TASK_QUEUE_TABLE_NAME = "taskQueue"
 
+const BOT_GUILD_EXPIRY_INDEX = "botID_shard_expireTime";
+
 class TaskQueue {
   private client: Eris.Client;
-  private restClient: Eris.Client;
   private db: BaseDB;
+  private shardCount: number;
+  private shardID: number;
 
-  constructor({ r, client, restClient }: { r: any, client: Eris.Client; restClient: Eris.Client }) {
+  constructor({ r, client, shardID, shardCount }: { r: any, client: Eris.Client; shardCount: number, shardID: number }) {
     this.client = client;
-    this.restClient = restClient;
+    this.shardCount = shardCount;
+    this.shardID = shardID;
     this.db = new BaseDB(r, TASK_QUEUE_TABLE_NAME);
     this.db.ensureTable(TASK_QUEUE_TABLE_NAME);
+    this.db.ensureComplexIndex(BOT_GUILD_EXPIRY_INDEX, table => table.indexCreate(BOT_GUILD_EXPIRY_INDEX, doc => [doc("botID"), doc("shiftedGuildID").mod(doc("shardCount")), doc("expireTime")]))
     this.processQueue = this.processQueue.bind(this);
     this.runExpiredTasks = this.runExpiredTasks.bind(this);
     this.removeQueueEntry = this.removeQueueEntry.bind(this);
     this.processTask = this.processTask.bind(this);
     this.incrementRetries = this.incrementRetries.bind(this);
-    if (!process.env.id || process.env.id === "0") {
-      setInterval(this.runExpiredTasks, 1000);
-    }
+    setInterval(this.runExpiredTasks, 1000);
   }
 
   runExpiredTasks() {
-    // @ts-ignore
-    this.db.r.table(TASK_QUEUE_TABLE_NAME).filter((r: any) => r("expireTime").le(this.db.r.now())).run().then(this.processQueue);
+    const botID = this.client.user.id;
+
+    this.db.r.table(TASK_QUEUE_TABLE_NAME).between([botID, this.shardID, this.db.r.minval], [botID, this.shardID, this.db.r.now()], { index: BOT_GUILD_EXPIRY_INDEX }).run().then(result => this.processQueue(result as unknown as TaskWithID[]));
   }
 
   processQueue(queue: TaskWithID[]) {
@@ -68,10 +80,10 @@ class TaskQueue {
     }
   }
 
-  executeTask(task: Task) {
+  executeTask(task: TaskWithID) {
     switch (task.action) {
       case "unmute":
-        return this.unmute(task.meta);
+        return this.unmute(task);
     }
     return Promise.reject("Unknown Task")
   }
@@ -89,7 +101,7 @@ class TaskQueue {
     }
   }
 
-  schedule(task: Task, timeInput: string | Date | number) {
+  schedule(task: RawTask, timeInput: string | Date | number) {
     let time: number;
     if (typeof timeInput === "string") {
       time = this.estimateEndDateFromString(timeInput).getTime() / 1000;
@@ -100,16 +112,28 @@ class TaskQueue {
     } else {
       throw new Error("Invalid type of time.");
     }
-    let datedTask = Object.assign({ expireTime: this.db.r.epochTime(time) }, task);
-    this.db.r.table(TASK_QUEUE_TABLE_NAME).insert(datedTask).run();
+    let datedTask = Object.assign({
+      expireTime: this.db.r.epochTime(time),
+      botID: this.client.user.id,
+      shardCount: this.shardCount,
+      shiftedGuildID: Number(BigInt(task.guildID) >> 22n),
+    }, task);
+    this.db.r.table(TASK_QUEUE_TABLE_NAME).insert(datedTask as ExtendedTask).run();
   }
 
-  async unmute(meta: any) {
+  async unmute(task: TaskWithID) {
     let options: { roles?: string[], mute?: boolean } = {};
+
+    const meta = task.meta;
+
+    if (!this.client.guilds.has(task.guildID)) {
+      console.error("Task scheduled on incorrect worker!!!!")
+    }
+
     if (meta.roleIDs) {
       let member;
       try {
-        member = await this.restClient.getRESTGuildMember(meta.guildID, meta.userID);
+        member = await this.client.getRESTGuildMember(meta.guildID, meta.userID);
       } catch (error) {
         Sentry.captureException(error);
         console.error("From this place", error);
